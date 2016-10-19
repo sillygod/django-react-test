ad12e870a00e6b8b4134084798251a1b167a861c
sillygod <sillygod@livemail.tw>
2016-10-19 15:54:15 +0800

    temporarily complete server

---
+++ frontend/helpers/ApiClient.jsx
@@ -0,0 +1,44 @@
+import superagent from 'superagent';
+import config from '../config';
+
+const method = ['get', 'post', 'put', 'patch', 'del']
+
+function formatUrl(path){
+    const adjustedPath = path[0] !== '/' ? '/' + path : path;
+
+    if(__SERVER__){
+        return 'http://' + config.apiHost + ':' + config.apiPort + adjustedPath;
+    }
+
+    return '/api' + adjustedPath;
+}
+
+
+export default class ApiClient {
+    constructor(req){
+        methods.forEach((method) =>
+            this[method] = (path, {params, data}={}) => new Promise((resolve, reject) => {
+                const request = superagent[method](formatUrl(path));
+
+                if (params){
+                    request.query(params);
+                }
+
+                if(__SERVER__ && req.get('cookie')){
+                    request.set('cookie', req.get('cookie'));
+                }
+
+                // wtf __SERVER__ comes?
+
+                if(data){
+                    request.send(data);
+                }
+
+                request.end((err, { body } = {}) => err ? reject(body || err) : resolve(body));
+
+            });
+        );
+    }
+
+    empty() {}
+}
--- frontend/server.jsx
+++ frontend/server.jsx
@@ -15,6 +15,7 @@ import { ReduxAsyncConnect, loadOnServer } from 'redux-async-connect';
 import createHistory from 'react-router/lib/createMemoryHistory';
 import {Provider} from 'react-redux';
 import getRoutes from './routes';
+import ApiClient from './helpers/ApiClient';
 import config from './config';
 
 
@@ -42,3 +43,82 @@ server.on('upgrade', (req, socket, head) =>{
     proxy.ws(req, socket, head);
 });
 
+
+proxy.on('error', (error, req, res) => {
+    let json;
+
+    if(error.code !== 'ECONNRESET'){
+        console.log('proxy error', error);
+    }
+
+    if(!res.headersSent){
+        res.writeHead(500, {'content-type': 'application/json'});
+    }
+
+    json = {error: 'proxy_error', reason: error.message};
+    res.end(JSON.stringify(json));
+});
+
+app.use((req, res) => {
+    if (__DEVELOPMENT__){
+        // Do not cache webpack stats: the script file would change since
+        // hot module replacement is enabled in the development env
+        webpackIsomorphicTools.refresh();
+    }
+
+    const client = new ApiClient(req);
+    const memoryHistory = createHistory(req.originalUrl);
+    const store = createStore(memoryHistory, client);
+    const history = syncHistoryWithStore(memoryHistory, store);
+
+    function hydrateOnClient(){
+        res.send('<!Doctype html>\n' + ReactDOM.renderToString(<Html assets={webpackIsomorphicTools.assets()} store={store}/>));
+    }
+
+    if(__DISABLE_SSR__){
+        hydrateOnClient();
+        return;
+    }
+
+    match({history, routes: getRoutes(store), location: req.originalUrl}, (error, redirectLocation, renderProps) => {
+        if(redirctLocation){
+            res.redirect(redirectLocation.pathname + redirectLocation.search);
+        } else if (error){
+            console.error('ROUTER ERROR:', pretty.render(error));
+            res.status(500);
+            hydrateOnClient();
+        } else if (renderProps){
+            loadOnServer({...renderProps, store, helpers: {client}}).then(()=>{
+                const component = (
+                    <Provider store={store} key="provider">
+                        <ReduxAsyncConnect {...renderProps} />
+                    </Provider>
+                );
+
+                res.status(200);
+
+                global.navigator = {userAgent: req.headers['user-agent']};
+
+                res.send('<!docktype html>\n' +
+                    ReactDOM.renderToString(<Html assets={webpackIsomorphicTools.assets()} component={component} store={store} />));
+            });
+        } else {
+            res.status(404).send('Not found');
+        }
+    });
+
+});
+
+
+if(config.port){
+    server.listen(config.port, (err) => {
+        if(err){
+            console.error(err);
+        }
+
+        console.info('----\n==> ✅  %s is running, talking to API server on %s.', config.app.title, config.apiPort);
+        console.info('==> 💻  Open http://%s:%s in a browser to view the app.', config.host, config.port);
+    });
+} else {
+    console.error('==>     ERROR: No PORT environment variable has been specified');
+}
