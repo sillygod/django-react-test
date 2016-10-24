3a9326d4331cbf3baa14326f97fc12790071bd0f
sillygod <sillygod@livemail.tw>
2016-10-24 22:25:25 +0800

    temp to commit

--- frontend/redux/create.js
+++ frontend/redux/create.js
@@ -1,2 +1,12 @@
 import { createStore as _createStore, applyMiddleware, compose } from 'redux';
 import createMiddleware from './middleware/clientMiddleware';
+import { routerMiddleware } from 'react-router-redux';
+
+export default function createStore(history, client, data){
+
+    const reduxRouterMiddleware = routerMiddleware(history);
+    const middleware = [createMiddleware(client), reduxRouterMiddleware];
+
+    let finalCreateStore;
+
+}
