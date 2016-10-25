7d09d550be463ca655299c01768790384d1f7def
sillygod <sillygod@livemail.tw>
2016-10-25 23:41:25 +0800

    work on create.js

--- frontend/redux/create.js
+++ frontend/redux/create.js
@@ -9,4 +9,15 @@ export default function createStore(history, client, data){
 
     let finalCreateStore;
 
+    if(__DEVELOPMENT__ && __CLIENT__ && __DEVTOOLS__){
+        const { persistState } = require('redux-devtools');
+        const DevTools = require('../containers/DevTools/DevTools');
+
+        finalCreateStore = compose(
+            applyMiddleware(...middleware),
+            window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument(),
+            persistState(window.location.href.match(/[?&]debug_session=([^&]+\b/))
+        )(_createStore);
+    }
+
 }
