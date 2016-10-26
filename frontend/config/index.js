108f00514c43f4fbc6e7e9c68ae774410bede5fa
GoreStarry <rx783gp03@gmail.com>
2016-10-26 17:07:05 +0800

    e2e env first commit

---
+++ frontend/config/index.js
@@ -0,0 +1,12 @@
+// see http://vuejs-templates.github.io/webpack for documentation.
+var path = require('path')
+
+module.exports = {
+  build: {
+  },
+  dev: {
+    port: 8080,
+    assetsPublicPath: '/',
+    assetsSubDirectory: 'static'
+  }
+}
